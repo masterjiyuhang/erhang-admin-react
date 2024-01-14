94c0458c77e8cfbee1aa8ed08876fe2a9cecf31c
纪宇航 <jiyuhang@jctrans.net>
2024-01-14 23:27:26 +0800

    feat: init mini react day 02

---
+++ mini-react-day-02/core/React.js
@@ -0,0 +1,46 @@
+const createTextNode = (textValue) => {
+  return {
+    type: 'TEXT_NODE',
+    props: {
+      innerText: textValue,
+      children: []
+    }
+  }
+}
+
+const createElement = (type, props, ...children) => {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((item) => {
+        return typeof item === 'string' ? createTextNode(item) : item
+      })
+    }
+  }
+}
+
+const render = (element, container) => {
+  const { type } = element
+  const vdom = type === 'TEXT_NODE' ? document.createElement('P') : document.createElement(type)
+
+  Object.keys(element.props).forEach((key) => {
+    if (key !== 'children') {
+      vdom[key] = element.props[key]
+    }
+  })
+
+  const children = element.props.children
+  children.forEach((child) => {
+    render(child, vdom)
+  })
+
+  container.append(vdom)
+}
+
+const React = {
+  render,
+  createElement
+}
+
+export default React
